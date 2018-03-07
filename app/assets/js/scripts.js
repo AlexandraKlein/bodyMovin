2009e5530774ccc4759225148c40bfcfcb218f75
ali.klein <alexandraeklein@yahoo.com>
2018-03-07 10:50:37 -0800

    hover events

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -7,26 +7,28 @@
  * Copyright 2018. MIT licensed.
  */
 var elementsArr = ['ambulance','apple','atoms','bandaid','bicycle','bottle','cross','eye','female_doctor','female_nurse','male_doctor','male_nurse','dropper','heart','mobile','pills','stethoscope','syringe','thermometer','tubes'];
+var $container = $('.bm_container');
+var anim = [];
 
-var element, anim = [];
-
-for(var i=0; i<elementsArr.length; i+=1){
-  element = document.getElementById(elementsArr[i]);
+for(var i=0; i<elementsArr.length; i++){
   var params = {
     container: document.getElementById(elementsArr[i]),
-    autoplay:false,
-    loop:false,
-    animationData:animations[elementsArr[i]],
-    renderer:'svg'
+    autoplay: false,
+    loop: false,
+    animationData: animations[elementsArr[i]],
+    renderer: 'svg'
   };
   anim[elementsArr[i]] = bodymovin.loadAnimation(params);
+  anim[elementsArr[i]].playSegments([0, 20], true);
 }
 
-
-$('.bm_container').mouseenter(function(){
-  anim[$(this).attr('id')].play();
+$container.mouseenter(function(){
+  anim[$(this).attr('id')].playSegments([20, 80], true);
 });
 
-$('.bm_container').mouseleave(function(){
-  anim[$(this).attr('id')].goToAndStop(0);
+$container.mouseleave(function(){
+  var currAnim = anim[$(this).attr('id')]
+  var curr = currAnim.currentRawFrame;
+  currAnim.setDirection(-1);
+  currAnim.goToAndPlay(curr, true);
 });
