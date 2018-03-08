44fe7708ef72108b077f660706e4b7767a7435d3
ali.klein <alexandraeklein@yahoo.com>
2018-03-08 13:27:49 -0800

    mouseover events on svg container

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -14,10 +14,22 @@ for(var i=0; i<elementsArr.length; i++){
   var params = {
     container: document.getElementById(elementsArr[i]),
     autoplay: false,
-    loop: true,
+    loop: false,
     animationData: animations[elementsArr[i]],
     renderer: 'svg'
   };
   anim[elementsArr[i]] = bodymovin.loadAnimation(params);
-  anim[elementsArr[i]].play();
+  anim[elementsArr[i]].playSegments([0, 50], true);
 }
+
+$container.mouseenter(function(){
+  anim[$(this).attr('id')].playSegments([50, 100], true);
+});
+
+$container.mouseleave(function(){
+  var currAnim = anim[$(this).attr('id')]
+  var curr = currAnim.currentRawFrame;
+  currAnim.setDirection(-1);
+  currAnim.goToAndPlay(curr, true);
+});
+
